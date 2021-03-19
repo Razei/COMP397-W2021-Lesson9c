d722fd5d129650566050f86cbc41fb4b5a815520
Razei <razeixyz@gmail.com>
2021-03-19 19:09:03 -0400

    Save player rotation

--- Assets/_Scripts/ControlPanelController.cs
+++ Assets/_Scripts/ControlPanelController.cs
@@ -109,6 +109,7 @@ public class ControlPanelController : MonoBehaviour
     {
         player.controller.enabled = false;
         player.transform.position = sceneData.playerPosition;
+        player.transform.rotation = sceneData.playerRotation;
         player.controller.enabled = true;
 
         int health = sceneData.playerHealth;
@@ -119,6 +120,7 @@ public class ControlPanelController : MonoBehaviour
     public void OnSaveButtonPressed()
     {
         sceneData.playerPosition = player.transform.position;
+        sceneData.playerRotation = player.transform.rotation;
         sceneData.playerHealth = player.health;
     }
 }
